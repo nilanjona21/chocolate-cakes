638a722bf97256c0646a9002c1936fc556b9ed49
nilanjona21 <74915768+nilanjona21@users.noreply.github.com>
2020-12-16 20:07:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+ways to make make a chocolate cake:
+take 4 cups of flour
+50 gms choco ChannelSplitterNode
+40gms butter
+2/1 cups milkk
+
+to make a batter:
+whip the things in a large bowl
+then put the things on buttersheet
+and bake
+
+then you have a chocolate cake with you!!
+
